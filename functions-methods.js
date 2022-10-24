f2cf1de4d66f41f55d20a102b4716622f3302e8a
Niodig <steur81@gmail.com>
2022-10-24 14:09:10 +0200

    Huiswerk opdracht Gideon

--- functions-methods.js
+++ functions-methods.js
@@ -9,8 +9,24 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+function getEmailDomain(emailadress) {
+    const indexOfAt = emailadress.indexOf('@');
+    //We willen de apenstaart vinden in een email adress.
+    const domain = emailadress.substring(indexOfAt +1);
+    //we willen de uitkomst loggen die na de apenstaart komt
 
+    return domain;
+}
 
+const domain1 = getEmailDomain('n.eeken@novi-education.nl');
+const domain2 = getEmailDomain('t.mellink@novi.nl');
+const domain3 = getEmailDomain('a.wiersma@outlook.com');
+
+console.log(domain1);
+console.log(domain2);
+console.log(domain3);
+
+// console.log(domain1, domain2, domain3)
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -20,7 +36,27 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail (emailadress) {
+    const indexOfAt = emailadress.indexOf('@');
+    const domain = emailadress.substring(indexOfAt +1);
+    console.log(domain);
+}
+if (domain === novi-education.nl) {
+    //code breekt hier, snap niet waarom deze niet werkt... Hij geeft aan "domain is not defined.
+    return 'Student';
+} else if (domain === novi.nl){
+    return 'Medewerker';
+} else {
+    return 'Extern';
+}
 
+const type1 = typeOfEmail('n.eeken@novi-education.nl');
+const type2 = typeOfEmail('t.mellink@novi.nl');
+const type3 = typeOfEmail('a.wiersma@outlook.com');
+
+console.log(type1);
+console.log(type2);
+console.log(type3);
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -34,3 +70,23 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity (emailadress) {
+    const containsAt = emailadress.includes('@');
+    const containsComma = emailadress.includes(',');
+    const indexOfFinalDot = emailadress.lastIndexOf('.')
+    const containsNoDot = indexOfFinalDot !== emailadress.length -1;
+    if (containsAt && containsComma && containsNoDot) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const mailOne = checkEmailValidity('n.eeken@novi.nl')
+const mailTwo = checkEmailValidity('tessmellink@novi.nl')
+const mailThree = checkEmailValidity('n.eekenanovi.nl')
+const mailFour = checkEmailValidity('n.eeken@novinl.')
+const mailFive = checkEmailValidity('tessmellink@novi,nl')
+
+console.log(mailOne, mailTwo, mailThree, mailFour, mailFive);
